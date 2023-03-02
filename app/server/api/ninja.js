c7ac27b747214b942fe3b75e4476e1353da8243b
Vladimir Ivanov <vgivanov@gmail.com>
2023-03-02 10:52:46 +0200

    Server Routes

--- app/server/api/ninja.js
+++ app/server/api/ninja.js
@@ -6,7 +6,7 @@ export default defineEventHandler(async (event) => {
     const { age } = await readBody(event); 
 
     //api call with private key
-    const { data } = await $fetch("https://api.currencyapi.com/v3/latest?apikey=FwaJbQqfDo3lUtHhEt9t4YtSgXtI1GkTaIttxWVm");
+    const { data } = await $fetch("https://api.currencyapi.com/v3/latest?apikey=eJdPhICJUDBt6B4e4IX6DZ4A9Xos4Z2JRZ8xh2v2");
 
   return {
     message: `Hello, ${name}! You are ${age} years old. Currency data: ${JSON.stringify(data)}`,
